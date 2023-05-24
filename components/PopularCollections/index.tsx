3e27ae127584749efdb5cb23c7afa5b6ae6bb9dc
Connor <wardconnor17@gmail.com>
2023-05-24 14:31:04 +0100

    add basic table layout

--- components/PopularCollections/index.tsx
+++ components/PopularCollections/index.tsx
@@ -4,24 +4,22 @@ import classes from "./popular-collections.module.css";
 
 type CollectionSchema =
   paths["/collections/v5"]["get"]["responses"]["200"]["schema"];
-type ChainCollections = Record<string, CollectionSchema>;
 
 const fetchCollections = async () => {
   //   Default to ethereum
   const chain = chains[0];
-  const response = await fetch(`${chain.reservoirBaseUrl}/collections/v5`, {
-    headers: {
-      "x-api-key": chain.apiKey || "",
-    },
-  });
-  const json = await response.json();
+  const response = await fetch(
+    `${chain.reservoirBaseUrl}/collections/v5?sortBy=1DayVolume`,
+    {
+      headers: {
+        "x-api-key": chain.apiKey || "",
+      },
+    }
+  );
+  const json = (await response.json()) as CollectionSchema;
 
   const { collections } = json;
   return collections;
-  //   return {
-  //     props: { ssr: { collections } },
-  //     revalidate: 5,
-  //   };
 };
 
 export default async function PopularCollections() {
@@ -31,13 +29,28 @@ export default async function PopularCollections() {
   return (
     <div>
       <div>PopularCollections</div>
-      <ul>
-        {collections.map((collection) => (
-          <li key={collection.slug}>
-            Name: {collection.name}. Volume(1day): {collection.volume["1day"]}
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Volume - 1 day</th>
+            <th>Volume - 7 day</th>
+            <th>Volume - 30 day</th>
+            <th>Volume - all time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {collections?.map((collection) => (
+            <tr key={collection.slug}>
+              <td>{collection.name}</td>
+              <td>{collection.volume?.["1day"]}</td>
+              <td>{collection.volume?.["7day"]}</td>
+              <td>{collection.volume?.["30day"]}</td>
+              <td>{collection.volume?.["allTime"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
