7c8a8d6c22922f213cb6615f6a454da6f724a29c
Connor <wardconnor17@gmail.com>
2023-05-24 19:42:04 +0100

    add basic token list

--- app/collection/[slug]/page.tsx
+++ app/collection/[slug]/page.tsx
@@ -4,5 +4,20 @@ import { notFound } from "next/navigation";
 export default async function Page({ params }: { params: { slug: string } }) {
   const tokens = await fetchTokens({ collection: params.slug });
   if (!tokens) notFound();
-  return <div>Collection</div>;
+
+  console.log(tokens[0].market);
+  return (
+    <div>
+      <div>Collection</div>
+      <ul>
+        {tokens.map(({ token, market }) => (
+          <li key={token?.tokenId}>
+            <img src={token?.image} width="100" />
+            {token?.name} - {market?.floorAsk?.price?.amount?.native}{" "}
+            {market?.floorAsk?.price?.currency?.symbol}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
