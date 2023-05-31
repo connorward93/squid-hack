28a0f9cd8dde9fc0c9664b485cb518d86b0c8ec7
Connor <wardconnor17@gmail.com>
2023-05-31 14:41:42 +0100

    add footer placeholder

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,13 @@
 import classes from "./footer.module.css";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={classes.container}>
+      <div>
+        <a href="https://github.com/connorward93/squid-hack">
+          Github repository↗
+        </a>
+      </div>
+    </footer>
+  );
 }
