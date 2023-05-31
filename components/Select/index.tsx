7721b38d68e450d52c672a20ba1e35a7009d6789
Connor <wardconnor17@gmail.com>
2023-05-31 23:51:33 +0100

    update select behaviour

--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -2,21 +2,30 @@ import ReactSelect from "react-select";
 import classes from "./select.module.css";
 
 type SelectProps = {
+  value?: any;
   options: { value: string | number; label: string }[];
   onChange: any;
 };
 
-export default function Select({ options, onChange }: SelectProps) {
+export default function Select({
+  value,
+  options,
+  onChange,
+  ...props
+}: SelectProps) {
   return (
     <ReactSelect
+      value={value}
       unstyled
       onChange={onChange}
       classNames={{
+        container: () => classes.container,
         control: () => classes.control,
         menu: () => classes.menu,
         option: () => classes.option,
       }}
       options={options}
+      {...props}
     />
   );
 }
