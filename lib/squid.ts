5f35f7005638041fcfa97a8224d3ede3980f3abb
Connor <wardconnor17@gmail.com>
2023-06-01 03:06:50 +0100

    remove testnet

--- lib/squid.ts
+++ lib/squid.ts
@@ -2,7 +2,7 @@ import { Squid } from "@0xsquid/sdk";
 
 const getSDK = (): Squid => {
   const squid = new Squid({
-    baseUrl: "https://testnet.api.0xsquid.com",
+    baseUrl: "https://api.0xsquid.com",
   });
   return squid;
 };
