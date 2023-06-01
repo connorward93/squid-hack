2c0eb99298090da84e1a71a18c5fee24808c952e
Connor <wardconnor17@gmail.com>
2023-06-01 03:07:06 +0100

    add squid routes

--- components/BuyModal/CurrencyView.tsx
+++ components/BuyModal/CurrencyView.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import BuyContext from "@/context/Buy";
-import { useAccount, useContractWrite, useFeeData } from "wagmi";
-import { prepareWriteContract, erc721ABI } from "@wagmi/core";
+import { useAccount, useBalance } from "wagmi";
+import { fetchBalance } from "@wagmi/core";
 import { ethers } from "ethers";
 import Modal from "./Modal";
 import Button from "../Button";
@@ -11,10 +11,11 @@ import chains from "@/constants/chains";
 import Select from "../Select";
 import classes from "./buy-modal.module.css";
 import Product from "./Product";
+import erc1155Abi from "@/abi/erc1155";
+import erc20Abi from "@/abi/erc20";
 
 export default function CurrencyView() {
   const params = useParams();
-  console.log(params);
   const {
     state: { item: token },
   } = useContext(BuyContext);
@@ -27,9 +28,11 @@ export default function CurrencyView() {
     tokens: null,
   });
   const [selectedToken, setSelectedToken] = useState<any>(null);
+  const [balance, setBalance] = useState<any>(0);
   const { address, isConnected } = useAccount();
+
   const chain = chains.find((chain) => chain.routePrefix === params.chainId);
-  console.log({ chain });
+
   useEffect(() => {
     if (!address || !squid) return;
     setLoading(false);
@@ -50,22 +53,167 @@ export default function CurrencyView() {
     });
   }, [address, token, squid]);
 
+  useEffect(() => {
+    if (!address) return;
+    (async () => {
+      try {
+        const balance = await fetchBalance({
+          address: address,
+          chainId: selectedChain?.chain,
+          token:
+            selectedToken &&
+            selectedToken.symbol !== selectedChain?.tokens?.[0].symbol
+              ? selectedToken.address
+              : null,
+        });
+
+        setBalance(balance);
+      } catch {
+        setBalance(0);
+      }
+    })();
+  }, [address, selectedChain, selectedToken]);
+
+  console.log({ token });
+
   const buyToken = async () => {
     const owner = token.token.owner;
+    const toToken = squid?.tokens.find(({ chainId, symbol }) => {
+      return (
+        chainId === chain?.id &&
+        symbol === token.market.floorAsk.price.currency.symbol
+      );
+    });
 
-    if (!address || !squid) return;
-    // write({ args: [owner, address, token.token.tokenId] });
+    if (!address || !squid || !chain || !toToken) return;
+
     // @ts-ignore
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const amount = "1000000000000000000";
-    // const { route } = await squid.getRoute({
-    //   toAddress: address,
-    //   fromChain: selectedToken.chainId,
-    //   fromToken: selectedToken.address,
-    //   fromAmount: amount,
-    //   toChain: "",
-    // });
+
+    const SquidCallType = {
+      DEFAULT: 0,
+      FULL_TOKEN_BALANCE: 1,
+      FULL_NATIVE_BALANCE: 2,
+      COLLECT_TOKEN_BALANCE: 3,
+    };
+
+    const erc20ContractInterface = new ethers.utils.Interface(erc20Abi);
+    const approveEncodeData = erc20ContractInterface.encodeFunctionData(
+      "approve",
+      [token.token.contract, "0"]
+    );
+
+    const squidMulticall = "0x4fd39C9E151e50580779bd04B1f7eCc310079fd3";
+
+    const erc1155Interface = new ethers.utils.Interface(erc1155Abi);
+
+    const buyNftEncodeData = erc1155Interface.encodeFunctionData(
+      "safeTransferFrom",
+      [
+        token.token.owner,
+        address,
+        token.token.tokenId,
+        token.market.floorAsk.price.amount.raw,
+        1,
+      ]
+    );
+
+    console.log(token);
+
+    const transferNftEncodeData = erc1155Interface.encodeFunctionData(
+      "safeTransferFrom",
+      [squidMulticall, address, 1, 1, 0x00]
+    );
+
+    const transferEncodeData = erc20ContractInterface.encodeFunctionData(
+      "transfer",
+      [address, "0"]
+    );
+
+    const { route } = await squid.getRoute({
+      toAddress: address,
+      fromChain: selectedToken.chainId,
+      fromToken: selectedToken.address,
+      fromAmount: token.market.floorAsk.price.amount.raw,
+      toChain: toToken?.chainId,
+      toToken: toToken?.address,
+      slippage: 1,
+      customContractCalls: [
+        {
+          callType: SquidCallType.FULL_TOKEN_BALANCE,
+          target: toToken?.address,
+          value: "0",
+          callData: approveEncodeData,
+          payload: {
+            tokenAddress: toToken?.address,
+            inputPos: 1,
+          },
+          estimatedGas: "50000",
+        },
+        {
+          callType: SquidCallType.DEFAULT,
+          target: token.token.contract,
+          value: "0",
+          callData: buyNftEncodeData,
+          payload: {
+            tokenAddress: "1",
+            inputPos: 1,
+          },
+          estimatedGas: "80000",
+        },
+        {
+          callType: SquidCallType.DEFAULT,
+          target: token.token.contract,
+          value: "0",
+          callData: transferNftEncodeData,
+          payload: {
+            tokenAddress: "0x",
+            inputPos: 1,
+          },
+          estimatedGas: "50000",
+        },
+        {
+          callType: SquidCallType.FULL_TOKEN_BALANCE,
+          target: toToken?.address,
+          value: "0",
+          callData: transferEncodeData,
+          payload: {
+            tokenAddress: toToken?.address,
+            inputPos: 1,
+          },
+          estimatedGas: "50000",
+        },
+      ],
+    });
+
+    const tx = (await squid.executeRoute({
+      signer,
+      route,
+    })) as ethers.providers.TransactionResponse;
+    const txReceipt = await tx.wait();
+
+    const axelarScanLink =
+      "https://axelarscan.io/gmp/" + txReceipt.transactionHash;
+    console.log(
+      "Finished! Please check Axelarscan for more details: ",
+      axelarScanLink,
+      "\n"
+    );
+
+    console.log(
+      "Track status via API call to: https://api.squidrouter.com/v1/status?transactionId=" +
+        txReceipt.transactionHash,
+      "\n"
+    );
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    const status = await squid.getStatus({
+      transactionId: txReceipt.transactionHash,
+    });
+
+    console.log("Status: ", status);
   };
 
   return (
@@ -119,7 +267,10 @@ export default function CurrencyView() {
                 }}
               />
             </div>
-            <div className={classes.price}>{}</div>
+            <div className={classes.price}>
+              <div>Min. required: -</div>
+              <div> Balance: {balance?.formatted || "-"}</div>
+            </div>
             <br />
             <div>
               <Button variant="squid" label="Buy token" onClick={buyToken} />
