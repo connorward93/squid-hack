1043b11e043a2e4112a50189479aa93f9fbf4075
Connor <wardconnor17@gmail.com>
2023-06-01 02:27:50 +0100

    add disable state to button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -6,23 +6,26 @@ type ButtonProps = {
   variant?: "primary" | "secondary" | "squid";
   onClick: MouseEventHandler<HTMLButtonElement>;
   label?: string | ReactNode;
+  disabled?: boolean;
 };
 
 export default function Button({
   variant = "primary",
   onClick,
   label,
+  disabled,
 }: ButtonProps) {
   switch (variant) {
     case "squid":
       return (
         <button
           type="button"
-          onClick={onClick}
+          onClick={!disabled ? onClick : undefined}
           className={clsx(
             classes.button,
             variant && classes[`button--${variant}`]
           )}
+          disabled={disabled}
         >
           {label || "Pay with Squid"}
         </button>
@@ -31,11 +34,12 @@ export default function Button({
       return (
         <button
           type="button"
-          onClick={onClick}
+          onClick={!disabled ? onClick : undefined}
           className={clsx(
             classes.button,
             variant && classes[`button--${variant}`]
           )}
+          disabled={disabled}
         >
           {label}
         </button>
